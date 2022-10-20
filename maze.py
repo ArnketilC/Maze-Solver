4f05c7794344c463b8772495c68d53072bd8f132
ArnketilC <cyril.anquetil@hotmail.fr>
2022-10-20 18:26:53 +0200

    Simple node check and clean up

--- maze.py
+++ maze.py
@@ -3,9 +3,11 @@ from node import Node
 import math
 
 maze = ''
+
+
 class Maze:
     def __init__(self, file) -> None:
-        """Init the maze"""
+        """Init the maze."""
         self.grid = [[]]
         self.start = ""
         self.end = ""
@@ -13,37 +15,55 @@ class Maze:
         self.__find_start_end()
 
     def __fillGrid(self, file) -> None:
-        """Fille the array of characters"""
+        """Fille the array of characters."""
         with open(file) as f:
             grid = f.readlines()
             self.grid = [[*line.rstrip()] for line in grid]
 
     def __find_start_end(self) -> None:
-        """Find start and end node"""
+        """Find start and end node."""
         for i, line in enumerate(self.grid):
             for j, value in enumerate(line):
-                if value ==  "x":
-                    self.grid[i][j] = Node(i,j,math.inf)
+                if value == "x":
+                    self.grid[i][j] = Node(i, j, math.inf)
                     self.grid[i][j].name = "B"
-                else :
-                    self.grid[i][j] = Node(i,j)
+                else:
+                    self.grid[i][j] = Node(i, j)
                     self.grid[i][j].name = f"{i}{j}"
                 if value == "$":
-                    self.start = Node(i,j)
+                    self.start = Node(i, j)
                     self.grid[i][j].name = "Start"
                 elif value == "&":
                     self.end = Node(i, j)
                     self.grid[i][j].name = "End"
 
-    def solve_w_AStar(self):
+    def __calculate_heuristic(self):
+        """Calculate heuristic for all node in the range."""
         for line in self.grid:
             for node in line:
                 if node.name != "End" and node.name != "B":
                     node.calculate_heuristic(self.end)
-                print(f"{node.name} ,{node.x}, {node.y}, {node.heuristic}")
+                print(f"Name:{node.name}, x:{node.x}, y:{node.y}, h:{node.heuristic}")
+
+    def __aStar(self, node) -> None:
+        """Check next other nodes."""
+        neighbourgs = []
+        for i in [-1, +1]:
+            for j in [-1, +1]:
+                try:
+                    checked = self.grid[node.x+i][node.y+j]
+                    if checked == self.end:
+                        return
+                    neighbourgs.append(checked)
+                except IndexError:
+                    pass
+        neighbourgs.sort(key=lambda x: x.heuristic, reverse=True)
+
+    def solve_w_AStar(self):
+        """Solve the maze using the A* algorithm."""
+        self.__calculate_heuristic()
+        self.__aStar(self.start)
 
     def solve_w_Dijkstra(self):
-        pass 
-
-
-
+        """Under construction."""
+        pass
--- solver.py
+++ solver.py
@@ -1,16 +1,19 @@
 from maze import Maze
 from time import time
 
+
 def time_it(foo, *args, **kwargs):
     start = time()
     foo(*args, **kwargs)
     end = time()
     print(f"The function took {end-start} sec")
 
+
 def main() -> None:
     maze = Maze('maze')
     print(type(maze.solve_w_AStar))
     maze.solve_w_AStar()
+
 
 if __name__ == '__main__':
     main()
