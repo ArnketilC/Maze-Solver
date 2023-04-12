9c6a0871b2cd82f5bffad7a2ae133af8906ea436
ArnketilC <cyril.anquetil@hotmail.fr>
2023-04-12 14:35:07 +0200

    Add timing function

--- maze.py
+++ maze.py
@@ -56,22 +56,24 @@ class Maze:
         """Check next other nodes."""
         cardinal = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         count = 0
-        for i, j in cardinal:
+        for i, j in cardinal: # Check all cardinal directions
             try:
                 target_node = self.grid[node.x + i][node.y + j]
                 if target_node.name != "End" and target_node.name != "B" and target_node.name != "Start":
-
-                    if target_node not in self.checked_list:
-                        self.checked_list.append(target_node)
-                        if target_node.heuristic == math.inf:
-                            target_node.calculate_heuristic(self.end)    
+                    # Skip if its the end, an obstacle or the start
+                    if target_node not in self.checked_list: 
+                        # Check if the node was already check for adjacency
+                        self.checked_list.append(target_node) # Add to the list for sorting
+                        if target_node.heuristic == math.inf : # Calculate heuristic if needed
+                            target_node.calculate_heuristic(self.end)  
                     if target_node.step_cost >= node.step_cost + 1 :
+                        # Change the cost to get to the target, if need be (not that usefull with step 1)
                         if node.last_node != target_node:
                             target_node.last_node = node
                             target_node.step_cost = node.step_cost + 1 
                             target_node.calculate_combine_heuristic()
-
                 elif target_node.name == "End":
+                    # If it's the end, no need to do previously mention steps
                     target_node.last_node = node
                     return True
                 else:
@@ -79,8 +81,9 @@ class Maze:
             except:
                 pass
         
-        # Sort everything in the list
+        # Add to the list of checked node
         self.already_checked.append(node)
+        # Sort the list of node base on the new heuristic
         self.checked_list.sort(key=lambda x: x.combine_heuristic) #, reverse=True)
 
         return False
@@ -90,28 +93,31 @@ class Maze:
         found_endpoint = False
         skip = False
         last_node = ''
-        count = 0
+
         while (found_endpoint is False):
+            # Loop until found endpoint or dead end.
             for i, node in enumerate(self.checked_list):
-                if node not in self.already_checked:
-                    still_checking = True   
+                if node not in self.already_checked: # Check for double checking.
+                    still_checking = True # To deal with dead end.
+                    # Check for adjacency.
                     found_endpoint = self.__check_adjacent_node(self.checked_list[i])
                     last_node = node
                     break
                 found_endpoint = False
 
+            # Contition for dead end.
             if still_checking is False:
                 found_endpoint = True
                 skip = True
             else:
                 still_checking = False
            
-        if skip is False:
+        if skip is False: # If end found
             step_list = [self.end, last_node]
             while (last_node.name != 'Start'):
                 last_node = last_node.last_node
                 step_list.append(last_node)
-        if skip is True:
+        if skip is True: # If dead end
             return False
 
         return step_list
@@ -122,11 +128,10 @@ class Maze:
 
     def solve_w_AStar(self):
         """Solve the maze using the A* algorithm."""
-        print(self.start.name)
-        self.__check_adjacent_node(self.start, debug=1)
-        solution = self.__aStar(self.checked_list[0])
+        self.__check_adjacent_node(self.start, debug=1) # Check for the first adjancy
+        solution = self.__aStar(self.checked_list[0]) # Start A* the first good adjancy
         if solution is not False:
-            solution.reverse()
+            solution.reverse() # Reverse list order for showing.
             for i, node in enumerate(solution):
                 print(f"Step {i}: {node.give_your_name()} ")
         else:
--- solver.py
+++ solver.py
@@ -2,17 +2,27 @@ from maze import Maze
 from time import time
 
 
-def time_it(foo, *args, **kwargs):
-    start = time()
-    foo(*args, **kwargs)
-    end = time()
-    print(f"The function took {end-start} sec")
+def time_it(foo):
+    """Timing of function."""
+    def wrapper(*args, **kwargs):
+        start = time()
+        foo(*args, **kwargs)
+        end = time()
+        print(f"The function took {end-start} sec")
+    return wrapper
+
+
+@time_it
+def a_star(maze):
+    """Simple wraper for A star."""
+    maze.solve_w_AStar()
 
 
 def main() -> None:
     maze = Maze('maze')
-    maze.solve_w_AStar()
-    print("End")
+    a_star(maze)
+
+    # print("End")
 
 
 if __name__ == '__main__':
